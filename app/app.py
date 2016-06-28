ae36601c171140340ef8a2093dfba3e1b33a25dd
Anshuman Patnaik <anshuman.patnaik2008@gmail.com>
2016-06-28 01:01:04 -0400

    fix flask static/template routes to mix with keren

--- app/app.py
+++ app/app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+## Override default template directory
+template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                            'js', 'dist')
+
+## Specify Static folder location
+## Hardcoded path for now since its just going to be an extension
+## of this folder
+static = "/js/dist"
+
+app = Flask(__name__,
+            template_folder=template_dir,
+            static_url_path=static)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
