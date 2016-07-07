c252c18bac404ea4182dacc3a860adbae79541c1
Keren Chandran <keren.chandran@gmail.com>
2016-07-06 23:44:24 -0400

    Adding built files

--- app/js/webpack.config.prod.js
+++ app/js/webpack.config.prod.js
@@ -38,6 +38,11 @@ export default {
         test: /(\.css|\.scss)$/,
         include: path.join(__dirname, 'src'),
         loader: ExtractTextPlugin.extract('css?sourceMap!sass?sourceMap')
+      },
+      {
+        test: /(\.less)$/,
+        include: path.join(__dirname, 'src'),
+        loader: ExtractTextPlugin.extract('css?sourceMap!postcss!less?sourceMap')
       }
     ]
   }
