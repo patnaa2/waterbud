356f95b4aefa83ca6a94294232a9c7e5f1821142
Anshuman Patnaik <anshuman.patnaik2008@gmail.com>
2016-07-10 18:51:11 -0400

    adding first api endpoint and a few more pip stuff

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
+from flask_restful import Resource, Api
 import os
+import pymongo
 
 ## Override default template directory
 template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
@@ -13,10 +15,24 @@ static = "/js/dist"
 app = Flask(__name__,
             template_folder=template_dir,
             static_url_path=static)
+api = Api(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+## API Endpoints
+class SensorData(Resource):
+
+    def get(self, location, start, end):
+        return {'location': location,
+                'start': start,
+                'end': end}
+
+
+## Add all API endpoints after declaring them
+api.add_resource(SensorData, '/data/<location>/<start>/<end>')
 
 if __name__ == '__main__':
     app.run(debug=True)
