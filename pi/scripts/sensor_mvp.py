88c81217e14e824145a4a80d8c763dd863b861b2
Anshuman Patnaik <anshuman.patnaik2008@gmail.com>
2016-07-12 23:26:26 -0400

    fixes to sensor_mvp code

--- pi/scripts/sensor_mvp.py
+++ pi/scripts/sensor_mvp.py
@@ -32,20 +32,20 @@ class Sensor(object):
 
     def init_GPIO(self):
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+        GPIO.setup(self.FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
         GPIO.add_event_detect(self.FLOW_SENSOR,
                               GPIO.FALLING,
                               callback=self.count_pulse)
 
-    def count_pulse(self):
+    def count_pulse(self, channel):
         self.count += 1
     
     def run(self):
         while True:
             try:
                 current_time = int(time.time() * 1000)
-                current_dt = int(datetime.datetime())
-                if current_time - self.old_time > 1000:
+                current_dt = datetime.datetime.now()
+                if (current_time - self.old_time) > 1000:
                     flow_rate = ((1000.0 / (current_time - self.old_time)) * self.count) / self.CALIBRATION_FACTOR
                     flow_ml = (flow_rate / 60) * 1000
 
@@ -53,13 +53,15 @@ class Sensor(object):
                         if self.debug:
                             print "Rate: %s" %(flow_rate)
                             print "Flow ml: %s" %(flow_ml)
-                            continue
                         self._ws.send("timestamp: %s, flowrate: %s" %(current_dt, 
-                                                                      flow_ml)
+                                                                      flow_ml))
+                    time.sleep(0.1)
                     self.old_time = int(time.time() * 1000)
+                    self.count = 0
             except KeyboardInterrupt:
                 try:
                     print "Keyboard interrupt. Exiting"
+                    GPIO.cleanup()
                     wc.close()
                 except:
                     pass
